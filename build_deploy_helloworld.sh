f7da0039a51125ae5cfa0ff3e383c0902bf65a6a
jameshardaker <jamesalanhardaker@gmail.com>
2023-06-19 22:25:23 +0100

    better build debug process

--- build_deploy_helloworld.sh
+++
@@ -1,3 +0,0 @@
-source sources/poky/oe-init-build-env qemu-build
-bitbake helloworld
-scp -r /home/jameshardaker/raspberryPi4/qemu-build/tmp-glibc/work/cortexa57-oe-linux/helloworld/0.1-r0/image/usr/bin/helloworld root@192.168.7.2:/usr/bin/
