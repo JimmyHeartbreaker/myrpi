d9482a29d5cf21dc30b18981dd613731c75a24f0
jameshardaker <jamesalanhardaker@gmail.com>
2023-06-20 13:45:27 +0100

    dont need this parameter

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,5 +1,4 @@
 echo "filename: $1";
-echo "workspaceFolder: $2";
-source $2/sources/poky/oe-init-build-env qemu-build
+source ./sources/poky/oe-init-build-env qemu-build
 bitbake $1
-scp -r $2/qemu-build/tmp-glibc/work/cortexa57-oe-linux/$1/0.1-r0/image/usr/bin/$1 root@192.168.7.2:/usr/bin/
+scp -r ./tmp-glibc/work/cortexa57-oe-linux/$1/0.1-r0/image/usr/bin/$1 root@192.168.7.2:/usr/bin/
