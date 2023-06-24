2e201ac66dc81c371646456a9d65902e008e7b87
jameshardaker <jamesalanhardaker@gmail.com>
2023-06-24 15:03:21 +0100

    commit

--- build_pi.sh
+++ build_pi.sh
@@ -5,4 +5,3 @@ sudo umount /dev/sdb2
 sudo bmaptool copy tmp-glibc/deploy/images/raspberrypi4-64/mypi-base-image-raspberrypi4-64.wic.bz2 /dev/sdb
 sudo mount /dev/sdb1 /media/boot
 sudo mount /dev/sdb2 /media/root
-jkhkh
