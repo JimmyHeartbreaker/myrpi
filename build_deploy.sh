651451ea9a83d4a33dab64d98d71c9ffd22fa1f7
jameshardaker <jamesalanhardaker@gmail.com>
2023-06-26 13:20:06 +0100

    painting

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,4 +1,4 @@
 echo "filename: $1";
 source ./sources/poky/oe-init-build-env qemu-build
 bitbake $1
-scp -r ./tmp-glibc/work/cortexa57-oe-linux/$1/0.1-r0/image/usr/bin/$1 root@192.168.7.2:/usr/bin/
+scp  -o StrictHostKeyChecking=no -r ./tmp-glibc/work/cortexa57-oe-linux/$1/0.1-r0/image/usr/bin/$1 root@192.168.7.2:/usr/bin/ 
