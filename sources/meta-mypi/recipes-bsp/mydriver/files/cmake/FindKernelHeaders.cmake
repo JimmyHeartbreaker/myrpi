87eb2819749e8426ef77d1d5f3f3c5228dba7387
jameshardaker <jamesalanhardaker@gmail.com>
2023-08-10 15:24:30 +0100

    sort out driver build through bitbake and vscode

--- sources/meta-mypi/recipes-bsp/mydriver/files/cmake/FindKernelHeaders.cmake
+++ sources/meta-mypi/recipes-bsp/mydriver/files/cmake/FindKernelHeaders.cmake
@@ -1,25 +1,9 @@
-# Find the kernel release
-execute_process(
-        COMMAND uname -r
-        OUTPUT_VARIABLE KERNEL_RELEASE
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-# Find the headers
-find_path(KERNELHEADERS_DIR
-        include/linux/user.h
-        PATHS /home/jameshardaker/raspberryPi4/rpi-build/tmp-glibc/work-shared/raspberrypi4-64/kernel-source/
-        NO_CMAKE_FIND_ROOT_PATH
-        )
-
-SET(KERNELHEADERS_DIR /home/jameshardaker/raspberryPi4/rpi-build/tmp-glibc/work-shared/raspberrypi4-64/kernel-build-artifacts/)
-message(STATUS "Kernel release: ${KERNEL_RELEASE}")
+SET(KERNELHEADERS_DIR ./../../../../../work-shared/raspberrypi4-64/kernel-build-artifacts/)
 message(STATUS "Kernel headers: ${KERNELHEADERS_DIR}")
 
 if (KERNELHEADERS_DIR)
     set(KERNELHEADERS_INCLUDE_DIRS
-    #    /home/jameshardaker/raspberryPi4/qemu-build/tmp-glibc/work-shared/qemuarm64/kernel-source/include/
-        /home/jameshardaker/raspberryPi4/rpi-build/tmp-glibc/work-shared/raspberrypi4-64/kernel-source/arch/arm64/include/
+    ./../../../../../work-shared/raspberrypi4-64/kernel-source/arch/arm64/include/
             CACHE PATH "Kernel headers include dirs"
             )
     set(KERNELHEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
