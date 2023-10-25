83be57322a61952c81e5442e2370a8d0e5524476
JimmyHeartbreaker <jamesalanhardaker@gmail.com>
2023-10-25 21:52:32 +0100

    fix file copy

--- deploy_qemu.sh
+++ deploy_qemu.sh
@@ -1,7 +1,7 @@
 echo "filename: $1";
 projectName=$(basename $1);
 echo $1/build/Target/$projectName
-
+ssh-keygen -R "127.0.0.1"
 scp -o StrictHostKeyChecking=no  -r $1/build/Target/${projectName} root@127.0.0.1:/usr/bin/ 
 scp -o StrictHostKeyChecking=no  -r $1/build/Target/${projectName}_Tests root@127.0.0.1:/usr/bin/ 
 #read -p "waiting for key input"
